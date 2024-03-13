5dca6b00e0c40e2f9a21d75b6fa44d5c459632f0
shojimoro <morodome.s.aa@gmail.com>
2024-03-13 11:05:24 +0900

    first commit

---
+++ REND_h5_B_W_V_W.py
@@ -0,0 +1,58 @@
+import os
+import numpy as np
+from flask import Flask, request, redirect, render_template, flash
+from werkzeug.utils import secure_filename
+from tensorflow.keras.models import Sequential, load_model
+from tensorflow.keras.preprocessing import image
+
+import cv2
+from tensorflow.keras.layers import Dense, Dropout, Flatten, Input
+
+
+
+classes = ["BENTONITE","WIRE","VINYL","WOOD"]
+image_size = 150
+
+UPLOAD_FOLDER = "uploads"
+ALLOWED_EXTENSIONS = set(['png', 'jpg', 'jpeg', 'gif'])
+
+app = Flask(__name__)
+
+def allowed_file(filename):
+    return '.' in filename and filename.rsplit('.', 1)[1].lower() in ALLOWED_EXTENSIONS
+
+model = load_model('./model.h5',compile=False)#学習済みモデルをロード
+
+
+@app.route('/', methods=['GET', 'POST'])
+def upload_file():
+    if request.method == 'POST':
+        if 'file' not in request.files:
+            flash('ファイルがありません')
+            return redirect(request.url)
+        file = request.files['file']
+        if file.filename == '':
+            flash('ファイルがありません')
+            return redirect(request.url)
+        if file and allowed_file(file.filename):
+            filename = secure_filename(file.filename)
+            file.save(os.path.join(UPLOAD_FOLDER, filename))
+            filepath = os.path.join(UPLOAD_FOLDER, filename)
+
+            #受け取った画像を読み込み、np形式に変換
+            img = cv2.imread(filepath)
+            b,g,r = cv2.split(img)
+            img = cv2.merge([r,g,b])
+            img = cv2.resize(img, (150,150))
+            #変換したデータをモデルに渡して予測する
+            result = model.predict(np.array([img]))
+            predicted = result.argmax()
+            pred_answer = "これは " + classes[predicted] + " です"
+
+            return render_template("index.html",answer=pred_answer)
+
+    return render_template("index.html",answer="")
+
+if __name__ == "__main__":
+    port = int(os.environ.get('PORT', 8080))
+    app.run(host ='0.0.0.0',port = port)
